022463350a7ae3e4d771689e1b835ec1c25b3854
Jay Garnes <jgarnes73@gmail.com>
2021-03-02 10:04:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Board from './Components/Board/Board'
+
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App;
